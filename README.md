bc2e6af819c3e00dfff3ebc50d4b540e80705afb
rin93 <ilama93@yandex.com>
2018-11-14 22:28:49 +0800

    Inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# majsoul_custom_bgm
+受贴吧小魔的魔改雀魂教程启发而尝试了自定义雀魂bgm。
+
+wiki：https://github.com/rin93/majsoul_custom_bgm/wiki
---
+++ start-server.py
@@ -0,0 +1,45 @@
+import http.client
+import http.server
+import socketserver
+import json
+import os
+
+try:
+	c = http.client.HTTPConnection("majsoul.union-game.com", timeout=2)
+
+	print('get latest version info... ', end='')
+	c.request('GET', '/0/version.json')
+	r = c.getresponse()
+	print('%s %s' % (r.status, r.reason))
+	a = r.read().decode('utf-8')
+	a = json.loads(a)
+
+	print('get code.js... ', end='')
+	c.request('GET', '/0/'+a['code'])
+	r = c.getresponse()
+	print('%s %s' % (r.status, r.reason))
+	a = r.read().decode('utf-8')
+
+	print('patching...')
+	a += open('bgm.js', 'r', encoding='utf8').read()
+
+	if not os.path.exists('web'):
+		os.makedirs('web')
+	open('web/code.patched.js', 'w', encoding='utf8').write(a)
+
+	print('done.\n')
+	print('starting http server...')
+
+	web_dir = os.path.join(os.path.dirname(__file__), 'web')
+	os.chdir(web_dir)
+
+	PORT = 8000
+	Handler = http.server.SimpleHTTPRequestHandler
+	httpd = socketserver.TCPServer(("", PORT), Handler)
+	print("http.server serving at port", PORT)
+	httpd.serve_forever()
+
+except Exception as e:
+	print(e.message if hasattr(e, 'message') else e)
+	print('\nerror occurred, please try again.')
+
